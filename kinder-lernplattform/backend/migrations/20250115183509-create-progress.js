d223d39a0f9764ffa9a44fb630324dd480fbad1e
smiluk87 <32164235+smiluk87@users.noreply.github.com>
2025-01-17 00:26:12 +0100

    userID als Fremdschlüssel in progress.js angepasst

--- kinder-lernplattform/backend/migrations/20250115183509-create-progress.js
+++ kinder-lernplattform/backend/migrations/20250115183509-create-progress.js
@@ -2,35 +2,42 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('progresses', {
+    await queryInterface.createTable('Progresses', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+      },
+      userId: { // Fremdschlüssel zu "users"
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users', // Name der referenzierten Tabelle
+          key: 'id',
+        },
+        onDelete: 'CASCADE', // Verhalten bei Löschung eines Benutzers
       },
       category: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       score: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       timestamp: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        field: 'createdat'
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        field: 'updatedat'
-      }
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('progresses');
-  }
+    await queryInterface.dropTable('Progresses');
+  },
 };
