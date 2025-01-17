655ec44ec75f76354bd510e9f0c0b473007b9c8b
smiluk87 <32164235+smiluk87@users.noreply.github.com>
2025-01-17 01:49:46 +0100

    code hinzugefügt in userController.js

---
+++ kinder-lernplattform/backend/controllers/userController.js
@@ -0,0 +1,19 @@
+const db = require('../models');
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await db.User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await db.User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
